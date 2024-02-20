4984b16bbd716f16dfce4b039044691a0edc195d
Osman <88623469+osmandayi@users.noreply.github.com>
2024-02-20 08:32:17 +0300

    initial

--- App.js
+++ App.js
@@ -2,15 +2,29 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/HomeScreen';
+import AllCourses from './screens/AllCourses';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ManageCourse from './screens/ManageCourse';
+import RecentCourses from './screens/RecentCourses';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function CourseOverview() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="RecentCourses" component={RecentCourses} />
+      <Tab.Screen name="AllCourses" component={AllCourses} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen options={{headerShown: false}} name="CourseOverview" component={CourseOverview} />
+        <Stack.Screen name="Manage" component={ManageCourse} />
       </Stack.Navigator>
     </NavigationContainer>
   );
